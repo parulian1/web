55503f7b763fa6fd512ed158a8fc333df3f33888
rio <rio.trilaksono@gramedia.digital>
2020-04-28 15:49:41 +0700

    remove import .env.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -8,7 +8,6 @@
 // This is useful for granularity you might need beyond just the environment.
 // Note that as usual, any environment variables you expose through it will end up in your
 // bundle, and you should not use it for any sensitive information like passwords or keys.
-import {env} from './.env';
 import {IEnvironment} from "@env/environment.interface";
 
 export const environment: IEnvironment = {
