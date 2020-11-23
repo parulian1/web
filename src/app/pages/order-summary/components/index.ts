40e92bd559f78f01c97715249acde621a7958318
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2020-11-23 14:43:43 +0700

    addd order summary manual transfer component

--- src/app/pages/order-summary/components/index.ts
+++ src/app/pages/order-summary/components/index.ts
@@ -1,2 +1,3 @@
 export * from './order-summary-done';
 export * from './order-summary-progress';
+export * from './order-summary-manual-transfer';
---
+++ src/app/pages/order-summary/components/order-summary-manual-transfer/order-summary-manual-transfer.component.ts
@@ -0,0 +1,125 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {OrderSummary, SummaryPayment} from "@app/models/checkout";
+
+@Component({
+  selector: "app-order-manual-transfer",
+  template: `
+    <div class="order-progress">
+      <div class="order-progress-header" style="margin-bottom: 20px">
+        <h2 class="title has-text-centered">
+          Selesaikan Pembayaran Anda
+        </h2>
+      </div>
+
+      <div class="order-progress-content">
+        <div class="order-progress--date-and-bank">
+          <div class="columns">
+            <div class="column is-4 is-offset-2">
+              <div class="box box-date">
+                <span class="due-date-title">Batas Pembayaran</span>
+                <span class="due-date-content">{{ payment.dateExpired | date: "d/MM/yyyy HH:mm" }}</span>
+              </div>
+            </div>
+
+            <div class="column is-4">
+            </div>
+          </div>
+        </div>
+
+        <div class="order-progress--info">
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <div class="box">
+                <h2 class="info-title">Informasi Pemesanan</h2>
+
+                <div class="info-desc">
+                  <div class="info-desc-item">
+                    <h4 class="item-header">Nomor Pesanan</h4>
+                    <p>{{ orderSummary.orderNumber }}</p>
+                  </div>
+                  <div class="info-desc-item">
+                    <h4 class="item-header">Tanggal Pemesanan</h4>
+                    <p>{{ orderSummary.created | date: "d MMMM y" }}</p>
+                  </div>
+                  <div class="info-desc-item">
+                    <h4 class="item-header">Total Belanja</h4>
+                    <p>{{ payment.amount | currency: "Rp ":"symbol":"1.0" }}</p>
+                  </div>
+                  <div class="info-desc-item">
+                    <h4 class="item-header">Metode Pembayaran</h4>
+                    <p>{{ payment.name }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="order-progress--actions">
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <div class="box has-text-centered">
+                <a class="button-cancel" [routerLink]="['/profile/orders/', orderSummary.orderNumber]">
+                  Batalkan Pesanan
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="order-progress--guide">
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <div class="box">
+                <h3 class="title guide-title">Petunjuk Pembayaran</h3>
+                <div class="guide-content">
+                  <p>Harap dicatat bahwa pesanan akan dibatalkan secara otomatis setelah 24 jam.</p>
+                  <p>Anda dapat membayar dengan melakukan transfer pada rekening berikut:</p>
+                  <ul>
+                    <li>{{ payment.name }} - {{ payment.accountHoldNumber }} - {{ payment.accountNumber }}</li>
+                  </ul>
+                  <p>Setelah anda melakukan transfer bank, harap beritahu kami dengan memasukkan detail transaksi.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="order-progress--confirmation">
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <div class="has-text-centered">
+                <a class="button-confirmation" [routerLink]="['/profile/orders/', orderSummary.orderNumber]">
+                  Konfirmasi Pembayaran
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./order-summary-manual-transfer.component.scss'],
+})
+export class OrderSummaryManualTransferComponent implements OnInit {
+  @Input() orderSummary: OrderSummary;
+
+  get payment(): SummaryPayment {
+    return this.orderSummary.payment;
+  }
+
+  constructor() {}
+  ngOnInit(): void {}
+
+
+  copyToClipboard(vaNumber: string) {
+    if (vaNumber) {
+      document.addEventListener("copy", (e: ClipboardEvent) => {
+        e.clipboardData.setData("text/plain", vaNumber);
+        e.preventDefault();
+        document.removeEventListener("copy", null);
+      });
+      document.execCommand("copy");
+    }
+  }
+}
