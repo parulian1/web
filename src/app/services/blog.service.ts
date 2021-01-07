01461e358aac713c10ee37ae221cb5e937e36a1f
Heri Priyatno <heri.priyatno@gramedia.digital>
2021-01-07 13:55:41 +0700

    fix: Remove unused code

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Blog, WpBlog} from "@app/models/blog.model";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Blog, WpBlog} from '@app/models/blog.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  private blogUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/marthatilaarblog.wordpress.com';
   private url = `/cms/blog/posts`;
-  private endpoint  = 'posts/';
 
   constructor(
     protected httpClient: HttpClient
