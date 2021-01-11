b8ee5d8b494cd5ffe67e905532b85fa3bc0f4085
Rio Trilaksono Putro <rio.trilaksono@staff.gramedia.com>
2021-01-11 13:52:09 +0700

    style: update styling for promotion list page

--- src/app/pages/promotion/promotion-list/promotion-list.component.ts
+++ src/app/pages/promotion/promotion-list/promotion-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductPromotionService} from '@app/services';
 import {PaginationService} from '@app/services/pagination.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -12,26 +12,30 @@ import {PagedResponse} from '@app/core/pagination';
 @Component({
   selector: 'app-promotion-list',
   template: `
+      <div class="container">
 
-    <div class="container">
-
-      <div class="card" *ngFor="let entity of entities">
-        <a [routerLink]="['/promo', entity.href|entityToSlug]">
-          <div class="card-item">
-            <img *ngIf="!!entity && !!entity.banner" [src]="entity?.banner" alt="{{entity.name}}" appImgResize>
-            <img *ngIf="!(entity && entity.banner)" [src]="this.defaultImageUrl" alt="{{entity.name}}" >
-            <div class="card-bottom">
-              <h3>{{entity.name}}</h3>
-              <p class="text-caption accent-dark">Periode {{entity.validFrom|date: 'dd LLLL yyyy'}} - {{entity.validTo|date: 'dd LLLL yyyy | HH:mm'}}</p>
-            </div>
+          <div class="card" *ngFor="let entity of entities">
+              <a [routerLink]="['/promo', entity.href|entityToSlug]">
+                  <div class="card-item">
+                      <img *ngIf="!!entity && !!entity.banner" [src]="entity?.banner" alt="{{entity.name}}"
+                           appImgResize>
+                      <img *ngIf="!(entity && entity.banner)" [src]="this.defaultImageUrl" alt="{{entity.name}}">
+                      <div class="card-bottom">
+                          <span class="title">{{entity.name}}</span>
+                          <p class="text-caption accent-dark">Periode {{entity.validFrom|date: 'dd LLLL yyyy'}}
+                              - {{entity.validTo|date: 'dd LLLL yyyy | HH:mm'}}</p>
+                      </div>
+                      <div class="card-button">
+                          <button class="btn-detail">Lihat Promo</button>
+                      </div>
+                  </div>
+              </a>
           </div>
-        </a>
       </div>
-    </div>
   `,
   styleUrls: ['./promotion-list.component.scss']
 })
-export class PromotionListComponent implements OnInit {
+export class PromotionListComponent implements OnInit, OnDestroy {
   readonly defaultImageUrl = 'assets/defaults/banner-hero.png';
 
   public entities: Array<ProductPromotion>;
@@ -44,7 +48,8 @@ export class PromotionListComponent implements OnInit {
               private route: ActivatedRoute,
               private router: Router,
               private title: Title,
-              private appConfigService: ConfigService) { }
+              private appConfigService: ConfigService) {
+  }
 
   ngOnInit(): void {
     this.subscription = this.route.data.subscribe((data: {
@@ -53,6 +58,10 @@ export class PromotionListComponent implements OnInit {
       this.entities = data.page.entities;
       this.pageNum = data.page.pageNumber;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
