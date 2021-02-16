befa1d3b6c5a7a49827e2d062e90095917c4f848
martogi.sitanggang <martogi@apps-foundry.com>
2021-02-17 00:07:47 +0700

    add validation just to prevent typescript error

--- src/app/shell/footer/payment.component.ts
+++ src/app/shell/footer/payment.component.ts
@@ -6,7 +6,7 @@ import { PaymentMethodType } from '@app/models/payment-method';
 @Component({
   selector: 'app-footer-payment',
   template: `
-    <div class="list" id="payment-footer">
+    <div class="list" id="payment-footer" *ngIf="childPaymentMethods.length > 0">
       <div class="title">
         <span>Pembayaran</span>
       </div>
@@ -32,7 +32,7 @@ export class PaymentComponent implements  OnInit {
   fetchPaymentMethods() {
     this.paymentMethodService.fetchList().subscribe((resp) => {
       resp.forEach((record) => {
-        record.paymentMethods.forEach((_paymentMethod) => {
+        record?.paymentMethods?.forEach((_paymentMethod) => {
           if (_paymentMethod.isActive) {
             this.childPaymentMethods.push(_paymentMethod);
           }
