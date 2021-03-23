852f239fd483e20049a11f93c9c575f8dde1d9ca
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2021-03-23 13:19:36 +0700

    [#87098] and [#87100] Order: add dialog confirmation to cancel order in order summary (progress)

--- src/app/pages/order-summary/components/order-summary-progress/order-summary-progress.component.ts
+++ src/app/pages/order-summary/components/order-summary-progress/order-summary-progress.component.ts
@@ -1,20 +1,33 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { OrderSummary, SummaryPayment } from "@app/models/checkout";
+import { Component, Input, OnInit } from '@angular/core';
+import { OrderSummary, SummaryPayment } from '@app/models/checkout';
+import { OrderCancelDialogComponent } from '@app/shared/order-cancel-dialog/order-cancel-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrderHistoryService } from '@app/services';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: "app-order-summary-progress",
-  templateUrl: "./order-summary-progress.component.html",
-  styleUrls: ["./order-summary-progress.component.scss"],
+  selector: 'app-order-summary-progress',
+  templateUrl: './order-summary-progress.component.html',
+  styleUrls: ['./order-summary-progress.component.scss'],
 })
 export class OrderSummaryProgressComponent implements OnInit {
   @Input() orderSummary: OrderSummary;
+  orderNumber: string;
 
   get payment(): SummaryPayment {
     return this.orderSummary.payment;
   }
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private orderHistory: OrderHistoryService,
+    private dialog: MatDialog,
+  ) { }
+
+  ngOnInit(): void {
+    this.orderNumber = this.route.snapshot.queryParams.order_id;
+  }
 
   //
   isVirtualAccount(): boolean {
@@ -24,12 +37,31 @@ export class OrderSummaryProgressComponent implements OnInit {
   }
   copyToClipboard(vaNumber: string) {
     if (vaNumber) {
-      document.addEventListener("copy", (e: ClipboardEvent) => {
-        e.clipboardData.setData("text/plain", vaNumber);
+      document.addEventListener('copy', (e: ClipboardEvent) => {
+        e.clipboardData.setData('text/plain', vaNumber);
         e.preventDefault();
-        document.removeEventListener("copy", null);
+        document.removeEventListener('copy', null);
       });
-      document.execCommand("copy");
+      document.execCommand('copy');
+    }
+  }
+
+  openOrderCancelDialog(): void {
+    const dialogRef = this.dialog.open(OrderCancelDialogComponent, {
+      // height: '10vh',
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe(({ isCancel }: { isCancel: boolean }) => {
+      if (isCancel) { this.onOrderCancel(); }
+    })
+  }
+  onOrderCancel(): void {
+    if (this.orderNumber) {
+      this.orderHistory.cancelOrder(this.orderNumber).subscribe(() => {
+        //
+        this.router.navigateByUrl(`profile/orders/${this.orderNumber}`)
+      });
     }
   }
 }
