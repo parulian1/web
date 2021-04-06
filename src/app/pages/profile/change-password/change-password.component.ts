35ed443a17846f1ba1f9073919c0d71dcfb8f2e8
Rio Trilaksono Putro <rio.trilaksono@gramedia.digital>
2021-04-06 11:32:59 +0700

    fix: add autocomplete off for old password input

--- src/app/pages/profile/change-password/change-password.component.ts
+++ src/app/pages/profile/change-password/change-password.component.ts
@@ -1,20 +1,20 @@
-import { Router } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { AlertDialogComponent } from "@app/shared/alert-dialog";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AlertDialogComponent } from '@app/shared/alert-dialog';
 
-import { AuthUserService } from "@app/services/auth-user.service";
+import { AuthUserService } from '@app/services/auth-user.service';
 
 @Component({
-  selector: "app-change-password",
-  templateUrl: "./change-password.component.html",
-  styleUrls: ["./change-password.component.scss"],
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss'],
 })
 export class ChangePasswordComponent implements OnInit {
   form: FormGroup;
-  submitted: boolean = false;
+  submitted = false;
 
   constructor(private service: AuthUserService, private router: Router, private snackbar: MatSnackBar) {}
 
@@ -24,9 +24,9 @@ export class ChangePasswordComponent implements OnInit {
 
   initForm(): void {
     this.form = new FormGroup({
-      oldPassword: new FormControl("", [Validators.required]),
-      password: new FormControl("", [Validators.required]),
-      passwordConfirm: new FormControl("", [Validators.required]),
+      oldPassword: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      passwordConfirm: new FormControl('', [Validators.required]),
     });
   }
   cleanForm(): void {
@@ -35,19 +35,19 @@ export class ChangePasswordComponent implements OnInit {
   }
 
   get oldPassword(): FormControl {
-    return this.form.get("oldPassword") as FormControl;
+    return this.form.get('oldPassword') as FormControl;
   }
   get password(): FormControl {
-    return this.form.get("password") as FormControl;
+    return this.form.get('password') as FormControl;
   }
   get passwordConfirm(): FormControl {
-    return this.form.get("passwordConfirm") as FormControl;
+    return this.form.get('passwordConfirm') as FormControl;
   }
 
   onSubmit() {
     this.submitted = true;
     if (this.form.valid) {
-      if (this.form.get("password").value == this.form.get("passwordConfirm").value) {
+      if (this.form.get('password').value === this.form.get('passwordConfirm').value) {
         const { oldPassword, password, passwordConfirm } = this.form.value;
         this.service
           .changePassword({
@@ -70,30 +70,30 @@ export class ChangePasswordComponent implements OnInit {
 
   goBack(): void {
     // todo: if form data is dirty, best to prompt the user first
-    this.router.navigate(["/profile"]);
+    this.router.navigate(['/profile']);
   }
 
   // Messages
-  showSuccessMessage(msg: string = "") {
-    const message = msg ? msg : "Berhasil Mengubah Password Anda.";
+  showSuccessMessage(msg: string = '') {
+    const message = msg ? msg : 'Berhasil Mengubah Password Anda.';
     const status = 201;
 
     this.snackbar.openFromComponent(AlertDialogComponent, {
       data: { message, status },
       duration: 3 * 1000, // 3 seconds
-      verticalPosition: "top",
-      panelClass: ["mt-alert--is-primary", "mt-alert--has-text-centered"],
+      verticalPosition: 'top',
+      panelClass: ['mt-alert--is-primary', 'mt-alert--has-text-centered'],
     });
   }
-  showErrorMessage(msg: string = "") {
-    const message = msg ? msg : "Terjadi kesalahan, silahkan periksa kembali inputan anda.";
+  showErrorMessage(msg: string = '') {
+    const message = msg ? msg : 'Terjadi kesalahan, silahkan periksa kembali inputan anda.';
     const status = 400;
 
     this.snackbar.openFromComponent(AlertDialogComponent, {
       data: { message, status },
       duration: 3 * 1000, // 3 seconds
-      verticalPosition: "top",
-      horizontalPosition: "right",
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
     });
   }
 
@@ -111,6 +111,6 @@ export class ChangePasswordComponent implements OnInit {
     });
   }
   _setErrorPasswordDoesntMatch(): void {
-    this.form.get("passwordConfirm").setErrors({ doesntMatch: true });
+    this.form.get('passwordConfirm').setErrors({ doesntMatch: true });
   }
 }
