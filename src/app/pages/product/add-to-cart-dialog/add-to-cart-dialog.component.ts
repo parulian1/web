1b4d06de9077db249f494151d65a9dc235249ca5
Rio Trilaksono Putro <rio.trilaksono@gramedia.digital>
2021-04-08 17:13:54 +0700

    fix: image add to cart

--- src/app/pages/product/add-to-cart-dialog/add-to-cart-dialog.component.ts
+++ src/app/pages/product/add-to-cart-dialog/add-to-cart-dialog.component.ts
@@ -1,6 +1,6 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {ProductDetail} from "@app/models/product-detail";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ProductDetail } from "@app/models/product-detail";
 
 @Component({
   selector: 'app-add-to-cart-dialog',
@@ -8,6 +8,9 @@ import {ProductDetail} from "@app/models/product-detail";
   styleUrls: ['./add-to-cart-dialog.component.scss']
 })
 export class AddToCartDialogComponent implements OnInit {
+
+  isProductImgAvailable = false;
+  productImgSrc = '';
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {
     product: string,
@@ -23,6 +26,15 @@ export class AddToCartDialogComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getProductImage(this.data.products);
+  }
+
+  getProductImage(products: ProductDetail) {
+    this.isProductImgAvailable = !!products.media && products.media.length > 0;
+
+    if (this.isProductImgAvailable) {
+      this.productImgSrc = products.media.find(product => product.type === 'image').image;
+    }
   }
 
 }
